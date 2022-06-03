9c87f9ff4917f50b0bb6417ec8f4ebfbb74d4d05
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2022-06-03 07:40:42 +0600

    feat(home): add services section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner/Banner";
 import OrderProcess from "./OrderProcess/OrderProcess";
+import Services from "./Services/Services";
 
 const Home = () => {
   return (
     <main>
       <Banner />
       <OrderProcess />
+      <Services />
     </main>
   );
 };
