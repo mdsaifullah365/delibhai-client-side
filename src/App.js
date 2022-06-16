ce8b07ba2ff759fafbf0a44cfce079392316345e
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2022-06-16 08:45:27 +0600

    fix(adminDashboard): update sidebar and link

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
-import './App.css';
-import Dashboard from './Pages/Dashboard/Dashboard';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import Registration from './Pages/Registration/Registration';
-import Footer from './Pages/Shared/Footer';
-import Navbar from './Pages/Shared/Navbar';
-import PageNotFound from './Pages/Shared/PageNotFound';
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Registration from "./Pages/Registration/Registration";
+import Footer from "./Pages/Shared/Footer";
+import Navbar from "./Pages/Shared/Navbar";
+import PageNotFound from "./Pages/Shared/PageNotFound";
 
 function App() {
   const path = useLocation().pathname;
   console.log(path);
   return (
     <>
-      {path !== '/adminPanel' && <Navbar />}
+      {path !== "/admin-dashboard" && <Navbar />}
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/registration' element={<Registration />} />
-        <Route path='/adminPanel' element={<Dashboard />} />
-        <Route path='/*' element={<PageNotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/admin-dashboard" element={<Dashboard />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
-      {path !== '/adminPanel' && <Footer />}
+      {path !== "/admin-dashboard" && <Footer />}
     </>
   );
 }
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import logo from "../../images/logo.png";
-import miniLogo from "../../images/miniLogo.jpg";
+import miniLogo from "../../images/miniLogo.svg";
 
 const Dashboard = () => {
   const [sidebar, setSidebar] = useState(false);
   return (
-    <div className="relative">
+    <div className="relative flex">
       <div
         className={`${
           sidebar ? "w-44 sm:w-0" : "w-0 sm:w-48 lg:w-64"
-        } bg-accent float-none md:float-left transition-all absolute md:relative duration-200 ease-in-out h-screen overflow-y-scroll scrollbar scrollbar-hidden hover:scrollbar-auto shadow-xl z-30 `}
+        } bg-gray-900 absolute md:relative transition-all duration-200 ease-in-out h-screen overflow-y-scroll scrollbar scrollbar-hidden hover:scrollbar-auto shadow-xl z-30 `}
       >
-        <div className="sticky top-0 bg-gray-700 mb-1 w-full text-base-100 text-center px-4 py-3 flex justify-between items-center">
+        <div className="sticky top-0 mb-1 w-full text-base-100 text-center px-4 py-3 flex justify-between items-center border-b border-gray-700">
           <img src={miniLogo} alt="Logo" className="w-12 rounded-full" />
           <div
             onClick={() => {
@@ -23,7 +23,7 @@ const Dashboard = () => {
           </div>
         </div>
 
-        <div className="absolute bottom-0 bg-gray-700 mt-1 w-full h-10 text-base-100 text-center p-2">
+        <div className="absolute bottom-0 mt-1 w-full h-10 text-base-100 text-center p-2">
           <img src={logo} alt="Logo" className="w-20" />
         </div>
       </div>
@@ -35,25 +35,30 @@ const Dashboard = () => {
               setSidebar(false);
             }
           }}
-          className="absolute right-0 left-0 top-0 bottom-0 bg-black opacity-10 md:hidden"
+          className={`${
+            sidebar ? "block md:hidden" : "hidden"
+          } absolute right-0 left-0 top-0 bottom-0 z-20 bg-black opacity-10`}
         ></div>
-        <div className="relative z-20 p-4">
-          <div className="w-full bg-gray-100 text-base-100 sticky left-0 top-0 p-4">
+        <div
+          className={`${"w-full"} relative z-10 transition-all duration-200 ease-in-out`}
+        >
+          <div className="w-full h-12 bg-gray-100 text-base-100 sticky left-0 top-0 flex justify-between items-center px-4">
             <button
               onClick={() => setSidebar(!sidebar)}
-              className="w-6 h-6 flex flex-col justify-evenly"
+              className={`${
+                sidebar
+                  ? "hidden sm:flex opacity-0 sm:opacity-100"
+                  : "flex sm:hidden opacity-100 sm:opacity-0"
+              } w-6 h-6 flex-col justify-evenly transition-opacity duration-200 ease-in-out`}
             >
               <div className="w-full h-1 bg-gray-900 rounded-3xl"></div>
               <div className="w-full h-1 bg-gray-900 rounded-3xl"></div>
               <div className="w-full h-1 bg-gray-900 rounded-3xl"></div>
             </button>
           </div>
-          <p>
-            Welcome To Dashboard Welcome To Dashboard Welcome To Dashboard
-            Welcome To Dashboard Welcome To Dashboard Welcome To Dashboard
-            Welcome To Dashboard Welcome To Dashboard Welcome To Dashboard
-            Welcome To Dashboard
-          </p>
+          <div className="flex justify-center items-center min-h-[calc(100vh-48px)]">
+            <p className="text-5xl text-center">Welcome to Admin Dashboard</p>
+          </div>
         </div>
       </div>
     </div>
