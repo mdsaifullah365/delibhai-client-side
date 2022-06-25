b5c8b99ae3ad39831bca46a032bd244dfd05a26b
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-06-25 22:36:03 +0600

    fix(delifood): make navmenu position sticky to the top

--- src/Pages/Delifood/Navmenu/Navmenu.js
+++ src/Pages/Delifood/Navmenu/Navmenu.js
@@ -1,42 +1,42 @@
-import React from "react";
-import CustomMenu from "./CustomMenu";
+import React from 'react';
+import CustomMenu from './CustomMenu';
 
 const Navmenu = () => {
   const menu = [
     {
       id: 1,
-      to: "/services/delifood",
-      text: "এখন পাবেন",
-      color: "010101",
+      to: '/services/delifood',
+      text: 'এখন পাবেন',
+      color: '010101',
     },
     {
       id: 2,
-      to: "/services/delifood/snacks",
-      text: "নাস্তা",
-      color: "7B1AB5",
+      to: '/services/delifood/snacks',
+      text: 'নাস্তা',
+      color: '7B1AB5',
     },
     {
       id: 3,
-      to: "/services/delifood/lunch",
-      text: "দুপুরের খাবার",
-      color: "F8A856",
+      to: '/services/delifood/lunch',
+      text: 'দুপুরের খাবার',
+      color: 'F8A856',
     },
     {
       id: 4,
-      to: "/services/delifood/evening-snacks",
-      text: "সন্ধার নাস্তা",
-      color: "00C797",
+      to: '/services/delifood/evening-snacks',
+      text: 'সন্ধার নাস্তা',
+      color: '00C797',
     },
     {
       id: 5,
-      to: "/services/delifood/juice",
-      text: "জুস",
-      color: "FF31A6",
+      to: '/services/delifood/juice',
+      text: 'জুস',
+      color: 'FF31A6',
     },
   ];
   return (
-    <nav className="mt-[23px] mb-[-2px] h-14 overflow-y-hidden">
-      <ul className="container flex gap-3 list-none flex-nowrap overflow-x-scroll whitespace-nowrap overflow-y-hidden pb-7 md:justify-center">
+    <nav className='mt-4 pt-2 mb-[-2px] h-14 overflow-y-hidden bg-white sticky top-12 z-50'>
+      <ul className='container flex gap-3 list-none flex-nowrap overflow-x-scroll whitespace-nowrap overflow-y-hidden pb-7 md:justify-center'>
         {menu.map((m) => (
           <CustomMenu key={m.id} to={m.to} color={m.color.toLowerCase()}>
             {m.text}
