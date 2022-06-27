0d70cf8bd4aa776a4a2f20c89884870082add297
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-06-27 22:39:03 +0600

    change loading spin style

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className='min-h-[calc(100vh-64px)] flex items-center justify-center '>
-      <div className='w-20 h-20 border-b-2 border-primary rounded-full animate-spin'></div>
+    <div className='min-h-[calc(100vh-64px)] flex justify-center pt-12'>
+      <div className='w-10 h-10 border-b-2 border-primary rounded-full animate-spin'></div>
     </div>
   );
 };
