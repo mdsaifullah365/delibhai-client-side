3b24d3e49589bd5e35c93c2421cd3fbf7e884669
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-06-28 22:05:40 +0600

    change navmenu z index

--- src/Pages/Delifood/Navmenu/Navmenu.js
+++ src/Pages/Delifood/Navmenu/Navmenu.js
@@ -12,7 +12,7 @@ const Navmenu = () => {
       .then((data) => setMenu(data));
   }, []);
   return (
-    <nav className='mt-4 pt-2 mb-[-2px] h-14 overflow-y-hidden bg-white sticky top-12 sm:top-24 z-50'>
+    <nav className='mt-4 pt-2 mb-[-2px] h-14 overflow-y-hidden bg-white sticky top-12 sm:top-24 z-40'>
       <ul className='container flex gap-3 list-none flex-nowrap overflow-x-scroll whitespace-nowrap overflow-y-hidden pb-7 md:justify-center'>
         <CustomMenu key='available' to='' color={'000000'}>
           এখন পাবেন
