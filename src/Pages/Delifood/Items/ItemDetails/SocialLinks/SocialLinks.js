4296ec209effe0673c25f35945adca8beed17edd
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2022-06-30 04:03:35 +0600

    feat(item details): add playstore icon

--- src/Pages/Delifood/Items/ItemDetails/SocialLinks/SocialLinks.js
+++ src/Pages/Delifood/Items/ItemDetails/SocialLinks/SocialLinks.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import app from '../../../../../files/delifood-App.apk';
 import facebook from '../../../../../images/social/facebook.svg';
 import youtube from '../../../../../images/social/youtube.svg';
+import playstore from '../../../../../images/social/playstore.png';
 
 const SocialLinks = () => {
   return (
-    <div className='bg-gradient-to-t from-white via-white'>
-      <h3 className='text-[4.266666vw] leading-[7.466666vw] lg:text-xl lg:leading-normal font-semibold mt-[6.4vw] mb-[2.666666vw] lg:mt-6 lg:mb-3 pt-12'>
+    <div className="bg-gradient-to-t from-white via-white">
+      <h3 className="text-[4.266666vw] leading-[7.466666vw] lg:text-xl lg:leading-normal font-semibold mt-[6.4vw] mb-[2.666666vw] lg:mt-6 lg:mb-3 pt-12">
         সোশাল মিডিয়া:
       </h3>
-      <div className='flex gap-[5.33333333vw] lg:gap-5 mb-[5.33333333vw] text-[9.333333vw] lg:mb-3 items-center'>
+      <div className="flex gap-[5.33333333vw] lg:gap-5 mb-[5.33333333vw] text-[9.333333vw] lg:mb-3 items-center">
         <a
-          href='https://www.facebook.com/delifoodbk'
-          target='_blank'
-          rel='noreferrer'>
-          <img src={facebook} className='w-[9.33333vw] lg:w-10' alt='' />
+          href="https://www.facebook.com/delifoodbk"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={facebook} className="w-[9.33333vw] lg:w-10" alt="" />
         </a>
         <a
-          href='https://www.youtube.com/channel/UCy1oRgxzbOzGYpKAuTB4NXQ'
-          target='_blank'
-          rel='noreferrer'>
-          <img src={youtube} className='w-[9.33333vw] lg:w-10' alt='' />
+          href="https://www.youtube.com/channel/UCy1oRgxzbOzGYpKAuTB4NXQ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={youtube} className="w-[9.33333vw] lg:w-10" alt="" />
+        </a>
+        <a href={app} download>
+          <img src={playstore} alt="" className="w-[9.33333vw] lg:w-10" />
         </a>
       </div>
     </div>
