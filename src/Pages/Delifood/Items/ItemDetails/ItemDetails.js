b88f59afd2b6b9d319cae2da6f4e854793ec6c42
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2022-07-01 02:32:18 +0600

    update favicon

--- src/Pages/Delifood/Items/ItemDetails/ItemDetails.js
+++ src/Pages/Delifood/Items/ItemDetails/ItemDetails.js
@@ -20,7 +20,7 @@ const ItemDetails = () => {
 
   const { name, img1, img2, video, price, description } = item;
   return (
-    <div className='pb-4 mb-12 lg:pb-0 lg:mb-0'>
+    <div className="pb-4 mb-12 lg:pb-0 lg:mb-0">
       <ItemCarousel
         img1={img1}
         img2={img2}
@@ -28,14 +28,14 @@ const ItemDetails = () => {
         price={price}
         name={name}
       />
-      <div className='container'>
-        <h2 className='inline-block bg-primary px-[2.9333333vw] py-[1.066666vw] lg:px-6 lg:py-3 rounded-lg text-white text-[4.266666vw] leading-[7.466666vw] lg:text-xl lg:leading-normal font-semibold mt-[6.4vw] mb-[2.666666vw] lg:mt-10 lg:mb-6'>
+      <div className="container">
+        <h2 className="inline-block bg-primary px-[2.9333333vw] py-[1.066666vw] lg:px-6 lg:py-3 rounded-lg text-white text-[4.266666vw] leading-[7.466666vw] lg:text-xl lg:leading-normal font-semibold mt-[6.4vw] mb-[2.666666vw] lg:mt-10 lg:mb-6">
           সংক্ষিপ্ত বর্ণনা
         </h2>
       </div>
       <div>
         <ItemDescription description={description} />
-        <div className='fixed lg:sticky bottom-0 left-0 right-0 container bg-gradient-to-t from-white via-white pb-3'>
+        <div className="fixed lg:sticky bottom-0 left-0 right-0 container bg-gradient-to-t from-white via-white pb-3">
           <SocialLinks />
           <OrderButton />
         </div>
--- src/Pages/Delifood/Items/Items.js
+++ src/Pages/Delifood/Items/Items.js
@@ -16,16 +16,17 @@ const Items = ({ gridView }) => {
     return <Loading />;
   }
   return (
-    <div className='container'>
+    <div className="container">
       <div
         className={`grid gap-x-[4vw] lg:gap-x-[2vw] gap-y-[5.333333vw] lg:gap-y-[2.6666vw] pb-4 mb-16 ${
           gridView ? 'grid-cols-2 lg:grid-cols-4' : 'grid-cols-1'
-        }`}>
+        }`}
+      >
         {items?.map((item) => (
           <ItemCard key={item._id} item={item} />
         ))}
       </div>
-      <div className='fixed lg:sticky bottom-3 left-0 right-0 container'>
+      <div className="fixed lg:sticky bottom-3 left-0 right-0 container">
         <OrderButton />
       </div>
     </div>
