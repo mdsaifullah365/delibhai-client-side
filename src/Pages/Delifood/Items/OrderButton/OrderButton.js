ef7d148be45c49932249f569bd8d6e271477786d
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2022-07-02 03:05:52 +0600

    add whatsapp link

--- src/Pages/Delifood/Items/OrderButton/OrderButton.js
+++ src/Pages/Delifood/Items/OrderButton/OrderButton.js
@@ -4,8 +4,9 @@ import { FaPhoneAlt } from 'react-icons/fa';
 const OrderButton = () => {
   return (
     <a
-      href='tel:01886232352'
-      className='btn btn-primary rounded-xl text-2xl leading-10 text-white flex justify-center items-center gap-[10px] w-full max-w-3xl mx-auto h-14'>
+      href="https://api.whatsapp.com/send?phone=01886232352"
+      className="btn btn-primary rounded-xl text-2xl leading-10 text-white flex justify-center items-center gap-[10px] w-full max-w-3xl mx-auto h-14"
+    >
       <FaPhoneAlt />
       <span>অর্ডার করুন</span>
     </a>
