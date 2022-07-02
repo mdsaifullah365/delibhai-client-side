9dd5b08a0159d2afc59db5557b34287107d24210
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-07-03 04:08:50 +0600

    update item card view

--- src/Pages/Delifood/Items/ItemCard.js
+++ src/Pages/Delifood/Items/ItemCard.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const ItemCard = ({ item, setItemModal }) => {
+const ItemCard = ({ item, setItemModal, gridView }) => {
   const { _id, img1, price, available, name } = item;
   return (
     <label
-      htmlFor="item-modal"
-      className="cursor-pointer"
-      onClick={() => setItemModal(item)}
-    >
+      htmlFor='item-modal'
+      className='cursor-pointer'
+      onClick={() => setItemModal(item)}>
       {/* Background Image */}
       <div
         key={_id}
@@ -18,33 +17,34 @@ const ItemCard = ({ item, setItemModal }) => {
           backgroundSize: 'cover',
           backgroundOrigin: 'padding-box',
         }}
-        className={`relative w-full h-[calc(50vw-32px)] lg:h-[calc(25vw-16px)] bg-[#bebebe] rounded-[2.6666666vw]`}
-      >
+        className={`relative w-full ${
+          gridView
+            ? 'h-[calc(48vw-1.5rem)] sm:h-[calc(320px-1.5rem-2vw)] md:h-[calc(384px-1.5rem-2vw)] lg:h-[calc(256px-0.75rem-1.5vw)] xl:h-[calc(320px-0.75rem-1.5vw)] 2xl:h-[calc(384px-0.75rem-1.5vw)]'
+            : 'h-[calc(100vw-3rem)] sm:h-[calc(640px-3rem)] md:h-[calc(768px-3rem)] lg:h-[calc(1024px-3rem)] xl:h-[calc(1280px-3rem)] 2xl:h-[calc(1536px-3rem)]'
+        } bg-[#bebebe] rounded-xl sm:rounded-2xl`}>
         {/* Dark Overlay */}
         <div
-          className={`absolute top-0 right-0 bottom-0 left-0 rounded-[2.6666666vw] bg-gradient-to-t from-black to-transparent opacity-75`}
-        ></div>
+          className={`absolute top-0 right-0 bottom-0 left-0 rounded-xl sm:rounded-2xl bg-gradient-to-t from-black to-transparent opacity-75`}></div>
         {/* Information */}
-        <div className="flex flex-col justify-between items-center text-white h-full relative p-[1.3333333vw]">
+        <div className='flex flex-col justify-between items-center text-white h-full relative p-[1.3333333vw]'>
           <div className={`w-full flex justify-between`}>
-            <p className="bg-white text-primary rounded-full px-[2.1333333vw] lg:lg:px-[1.066666vw] py-[0.533333vw] lg:py-[0.26666vw] text-[3.733333vw] lg:text-[1.8666vw] leading-[6.1333333vw] lg:leading-[3.0666vw] font-bold">
+            <p className='bg-white text-primary rounded-full px-[2.1333333vw] lg:lg:px-[1.066666vw] py-[0.533333vw] lg:py-[0.26666vw] text-[3.733333vw] lg:text-[1.8666vw] leading-[6.1333333vw] lg:leading-[3.0666vw] font-bold'>
               {price} টাকা
             </p>
-            <div className="bg-[rgba(0,0,0,0.4)] text-[3.2vw] lg:text-[1.6vw] rounded-full px-[2.1333333vw] lg:lg:px-[1.066666vw] py-[0.533333vw] lg:py-[0.26666vw] flex justify-between items-center gap-[1.3333333vw] lg:gap-[0.666666vw]">
+            <div className='bg-[rgba(0,0,0,0.4)] text-[3.2vw] lg:text-[1.6vw] rounded-full px-[2.1333333vw] lg:lg:px-[1.066666vw] py-[0.533333vw] lg:py-[0.26666vw] flex justify-between items-center gap-[1.3333333vw] lg:gap-[0.666666vw]'>
               <div
                 className={`w-[1.866666vw] h-[1.866666vw] lg:w-[0.933333vw] lg:h-[0.933333vw] rounded-full ${
                   available ? 'bg-[#03D7A1]' : 'bg-[#FFD600]'
-                }`}
-              ></div>
-              <div className="text-white leading-[5.06666vw] lg:leading-[2.53333vw] font-medium">
+                }`}></div>
+              <div className='text-white leading-[5.06666vw] lg:leading-[2.53333vw] font-medium'>
                 {available ? 'আছে' : 'নাই'}
               </div>
             </div>
           </div>
           {item?.titleImage ? (
-            <img src={item.titleImage} alt="" />
+            <img src={item.titleImage} alt='' />
           ) : (
-            <div className="text-[5.86666vw] lg:text-[2.93333vw] leading-[9.6vw] lg:leading-[4.8vw] font-bold">
+            <div className='text-[5.86666vw] lg:text-[2.93333vw] leading-[9.6vw] lg:leading-[4.8vw] font-bold'>
               {name}
             </div>
           )}
--- src/Pages/Delifood/Items/Items.js
+++ src/Pages/Delifood/Items/Items.js
@@ -24,24 +24,28 @@ const Items = ({ gridView }) => {
     document.body.classList.remove('no-scroll');
   }
   return (
-    <div className="container">
+    <div className='container'>
       {items.length === 0 ? (
-        <div className="text-center text-2xl lg:text-4xl pt-4 pb-16">
+        <div className='text-center text-2xl lg:text-4xl pt-4 pb-16'>
           No Items Found
         </div>
       ) : (
         <div
           className={`grid gap-x-[4vw] lg:gap-x-[2vw] gap-y-[5.333333vw] lg:gap-y-[2.6666vw] pb-4 mb-16 ${
             gridView ? 'grid-cols-2 lg:grid-cols-4' : 'grid-cols-1'
-          }`}
-        >
+          }`}>
           {items?.map((item) => (
-            <ItemCard key={item._id} item={item} setItemModal={setItemModal} />
+            <ItemCard
+              key={item._id}
+              item={item}
+              setItemModal={setItemModal}
+              gridView={gridView}
+            />
           ))}
         </div>
       )}
       {itemModal && <ItemModal item={itemModal} setItemModal={setItemModal} />}
-      <div className="fixed lg:sticky bottom-3 left-0 right-0 container">
+      <div className='fixed lg:sticky bottom-3 left-0 right-0 container'>
         <OrderButton />
       </div>
     </div>
