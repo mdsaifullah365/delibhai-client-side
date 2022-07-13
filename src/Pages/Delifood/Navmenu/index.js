d07806cdfadcf2ab4e4ff36f064b5329c8d14be1
mdsaifullah-wd <mdsaifullah.wd@gmail.com>
2022-07-13 18:14:59 +0600

    fix item category loading problem

--- src/Pages/Delifood/Navmenu/index.js
+++ src/Pages/Delifood/Navmenu/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useQuery } from 'react-query';
 import CustomMenu from './CustomMenu';
+import Loading from '../../Shared/Loading';
 
 const Navmenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    const url = `https://delibhai.herokuapp.com/delifood/category`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setMenu(data));
-  }, []);
+  const url = `https://delibhai.herokuapp.com/delifood/category`;
+  const { data: menu, isLoading } = useQuery(['menu'], () =>
+    fetch(url).then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
-    <nav className='mt-4 pt-2 mb-[-2px] h-14 overflow-y-hidden bg-white sticky top-12 sm:top-24 z-40'>
-      <ul className='container flex gap-3 list-none flex-nowrap overflow-x-scroll whitespace-nowrap overflow-y-hidden pb-7 md:justify-center'>
-        <CustomMenu key='available' to='' color={'000000'}>
+    <nav className="mt-4 pt-2 mb-[-2px] h-14 overflow-y-hidden bg-white sticky top-12 sm:top-24 z-40">
+      <ul className="container flex gap-3 list-none flex-nowrap overflow-x-scroll whitespace-nowrap overflow-y-hidden pb-7 md:justify-center">
+        <CustomMenu key="available" to="" color={'000000'}>
           এখন পাবেন
         </CustomMenu>
-        {menu.map((m) => (
+        {menu?.map((m) => (
           <CustomMenu key={m._id} to={m.value} color={m.color.toLowerCase()}>
             {m.label}
           </CustomMenu>
